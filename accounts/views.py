b4a60e57946802a1d83dc1948544e910f337cf1c
Stephen Graham <sgraham42@gmail.com>
2020-10-29 14:17:20 -0700

    moved static and base templates to project root

--- accounts/views.py
+++ accounts/views.py
@@ -6,7 +6,10 @@ from django.shortcuts import render, redirect
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    if request.user.is_authenticated:
+        return redirect('/accounts/profile')
+    else:
+        return render(request, 'index.html')
 
 def profile(request):
     context = {
