cde7e3fa593f4ac3dc252c5b5bade5f0ad04f867
Stephen Graham <sgraham42@gmail.com>
2020-10-29 14:59:39 -0700

    added 'initiative' app - next is models'

--- initiative_tracker/urls.py
+++ initiative_tracker/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('initiative/', include('initiative.urls')),
     path('admin/', admin.site.urls),
 ]
